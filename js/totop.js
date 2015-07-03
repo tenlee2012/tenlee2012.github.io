310b95f7d3680490c4e6b3ebdaddc97aa8161f5b
tenlee <1182964797@qq.com>
2015-07-03 16:38:24 +0800

    Site updated: 2015-07-03 16:38:24

--- js/totop.js
+++ js/totop.js
@@ -2,7 +2,7 @@
     
     // When to show the scroll link
     // higher number = scroll link appears further down the page    
-    var upperLimit = 1000; 
+    var upperLimit = 500; 
         
     // Our scroll link element
     var scrollElem = $('#totop');
